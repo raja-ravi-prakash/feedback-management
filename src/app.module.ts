f66b950cd5df910eaf557aed03c0e165368fe4b4
raja-ravi-prakash <iamraviprakash1@gmail.com>
2021-08-02 00:04:59 +0530

    deployment changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { EmployeeSchema, FacultySchema, ParentsSchema, StudentsSchema } from './feedback/feedback.model';
 import { FeedBackController } from './feedback/feedback.controller';
 import { FeedbackService } from './feedback/feedback.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     MongooseModule.forFeature([
       {
